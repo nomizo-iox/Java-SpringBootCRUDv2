9293402f52483fc3ecba79f2b8df163b5aeaa9df
nomizo-iox <nomizo.iox@gmail.com>
2020-02-29 08:15:56 -0600

    Created productService and things still work.

--- src/main/java/com/nomizo/db/ProductRepository.java
+++ src/main/java/com/nomizo/db/ProductRepository.java
@@ -5,4 +5,6 @@ import com.nomizo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    Product findByName (String name);
 }
